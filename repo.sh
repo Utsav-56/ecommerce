d65a4c48651aa8be52c46d68231b809d4fb46da3
Utsav-56 <utsavpokhrel200@gmail.com>
2026-04-27 23:15:21 +0545

    Added esewa

--- repo.sh
+++ repo.sh
@@ -1,17 +1,19 @@
-# Set your starting point (e.g., roughly 80 days ago)
+#!/bin/bash
+
+# Set your starting point (roughly 80 days ago)
 BASE_TIMESTAMP=$(date -d "80 days ago" +%s)
 
 while true; do
-  # 1. Add a random gap between commits (e.g., 1 to 4 days forward)
-  # 86400 seconds = 1 day, 345600 seconds = 4 days
+  # 1. Add a random gap between commits (1 to 4 days forward)
   RANDOM_GAP=$(shuf -i 86400-345600 -n 1)
   BASE_TIMESTAMP=$((BASE_TIMESTAMP + RANDOM_GAP))
   
   # 2. Format the new chronological date
   NEW_DATE=$(date -d "@$BASE_TIMESTAMP" +"%Y-%m-%d %H:%M:%S")
   
-  # 3. Overwrite the author, email, and the progressive date
-  git commit --amend --overwrite-date \
+  # 3. Overwrite the author, email, author date, and committer date cleanly
+  export GIT_COMMITTER_DATE="$NEW_DATE"
+  git commit --amend \
     --author="Utsav-56 <utsavpokhrel200@gmail.com>" \
     --no-edit \
     --date="$NEW_DATE"
